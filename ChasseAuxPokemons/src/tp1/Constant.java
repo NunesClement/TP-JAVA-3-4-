093b891ade51627ff29e98571011f15facc37c68
Minemega19 <themega19@gmail.com>
2018-03-30 16:39:45 +0200

    Add files via upload

--- ChasseAuxPokemons/src/tp1/Constant.java
+++ ChasseAuxPokemons/src/tp1/Constant.java
@@ -3,4 +3,5 @@ package tp1;
 
 public abstract class Constant {
 	public static final String[] LIST_TYPEPOKEMON = new String[] {"FEU","EAU","ELECTRICITE","PLANTE","GLACE","DRAGON","FONCE","TELEPATIQUE"};
+	public static final String[] LIST_POKEDEX = new String[] {"Pikachu","MewTwo","Dracaufeu","Lucario","Evoli","Ectoplasma"};
 }
